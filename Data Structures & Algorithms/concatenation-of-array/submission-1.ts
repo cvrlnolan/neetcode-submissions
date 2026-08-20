3c165b42ad2873617e2ff1665878dc0674908e88
George <42778495+cvrlnolan@users.noreply.github.com>
2026-08-20 14:22:20 +0100

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums: number[]): number[] {
+        let ans = [];
+
+        for (let i = 0; i < nums.length * 2; i++) {
+            ans[i] = i < nums.length ? nums[i] : nums[i - nums.length];
+        }
+
+        return ans;
+    }
+}
