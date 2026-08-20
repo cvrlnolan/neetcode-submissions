d7e4b9f610d0357b3b7a5286f895b35163cad77a
George <42778495+cvrlnolan@users.noreply.github.com>
2026-08-20 12:22:01 +0100

    Add: max-consecutive-ones - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums: number[]): number {
+        let count = 0
+        let max = 0
+        for (let i = 0; i<nums.length; i++) {
+            if (nums[i] === 1) {
+                count++
+                max = Math.max(max, count)
+            } else {
+                count = 0
+            }
+        }
+
+        return max
+    }
+}
