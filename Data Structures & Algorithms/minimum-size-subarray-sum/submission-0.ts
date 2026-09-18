296ea4d98367eddc90f1704671a779515a16244c
George <42778495+cvrlnolan@users.noreply.github.com>
2026-09-18 10:36:01 +0100

    Add: minimum-size-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-0.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} nums
+     * @return {number}
+     */
+    minSubArrayLen(target: number, nums: number[]): number {
+        let L = 0,
+            total = 0,
+            length = Infinity;
+
+        for (let R = 0; R < nums.length; R++) {
+            total += nums[R];
+            while (total >= target) {
+                length = Math.min(R - L + 1, length);
+                total -= nums[L];
+                L++;
+            }
+        }
+
+        return length === Infinity ? 0 : length;
+    }
+}
